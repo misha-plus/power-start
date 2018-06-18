f751fdbabdc7d90f7c6efdb7f1e9bd54e5e8c28e
Michael Plusnin <michael.plusnin@gmail.com>
2018-06-18 18:41:14 +0500

    Add agent

---
+++ agent/agent.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func sendBeacon() {
+	// TODO extract url
+	// TODO timeout
+	resp, err := http.DefaultClient.Post(
+		"http://localhost:3000/agent/thename/heartbeat", "", nil)
+	if err != nil {
+		log.Printf("Heartbeat error: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Heartbeat error: status = %d", resp.StatusCode)
+	}
+}
+
+func main() {
+	// TODO extract interval
+	ticker := time.NewTicker(5 * time.Second)
+	fmt.Println("Started")
+	for range ticker.C {
+		sendBeacon()
+	}
+}
