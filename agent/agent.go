618a34d202932ca3b188cec0951d32b08575ec8d
Michael Plusnin <michael.plusnin@gmail.com>
2018-06-20 07:53:09 +0500

    Add config to agent

--- agent/agent.go
+++ agent/agent.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"time"
+
+	"github.com/go-ini/ini"
 )
 
+var serverBaseURL = "http://localhost:3000"
+var machineName = "thename"
+var interval = 5 * time.Second
+
+var config = struct {
+	ServerBaseURL   string
+	MachineName     string
+	IntervalSeconds int
+}{}
+
 func sendBeacon() {
-	// TODO extract url
 	// TODO timeout
 	// TODO auth
-	resp, err := http.DefaultClient.Post(
-		"http://localhost:3000/agent/thename/heartbeat", "", nil)
+	url := fmt.Sprintf(
+		"%s/agent/%s/heartbeat", config.ServerBaseURL, config.MachineName)
+	resp, err := http.DefaultClient.Post(url, "", nil)
 	if err != nil {
 		log.Printf("Heartbeat error: %v", err)
 		return
@@ -33,20 +49,44 @@ func sendBeacon() {
 		return
 	}
 
-	if data.ShouldShutdown {
-		shutdown()
+	if !data.ShouldShutdown {
+		return
+	}
+
+	err = shutdown()
+	if err != nil {
+		fmt.Printf("Can't shutdown: %v", err)
 	}
 }
 
-func shutdown() {
-	println("=====================")
-	println("Shutting down")
-	println("=====================")
+func shutdown() error {
+	fmt.Println("Shutting down")
+	cmd := exec.Command("echo", "Hello")
+
+	stdout, err := cmd.StdoutPipe()
+	defer stdout.Close()
+	if err != nil {
+		return err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	defer stderr.Close()
+	if err != nil {
+		return err
+	}
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
+
+	return cmd.Run()
 }
 
 func main() {
-	// TODO extract interval
-	ticker := time.NewTicker(5 * time.Second)
+	err := ini.MapTo(&config, "agent-config.ini")
+	if err != nil {
+		log.Fatalf("Can't parse config: %v", err)
+	}
+
+	ticker := time.NewTicker(time.Duration(config.IntervalSeconds) * time.Second)
 	log.Println("Started")
 	for range ticker.C {
 		sendBeacon()
